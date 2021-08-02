eb5f0e24a5cae5b2aad80fb6afe454cddeb1068f
chichow <orzhouzhi@qq.com>
2021-08-02 21:19:38 +0800

    add bash support

--- cddir.sh
+++ cddir.sh
@@ -4,24 +4,29 @@ find_prefix="find -L . -mount -mindepth 1 \\( "
 find_suffix=" -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune -o -type d -print 2> /dev/null | fzf"
 
 function better_alt_c(){
-
 	# get exclude files from ./.ignore
-	exclude=""
-	while read str; do
-		exclude="$exclude -path '*$str*' -o "
-	done <$pattern_file
-	
-	# echo "$find_prefix$exclude$find_suffix"
+	local mypath exclude=""
+	if [[ -f "$pattern_file" ]];then
+		while read str; do
+			exclude="$exclude -path '*$str*' -o "
+		done <$pattern_file
+	fi
 
+	
 	mypath=$(eval ${find_prefix}${exclude}${find_suffix})
 
-	if (( $? == 0))
-	then
-		cd $mypath
+	if [[ $? == 0 ]];then
+		$(printf 'cd %q' "$mypath")
 	fi
-	zle reset-prompt
 
+	if [[ $SHELL == "/bin/zsh" ]];then
+		zle reset-prompt
+	fi
 }
 
-zle -N better_alt_c
-bindkey '\et' better_alt_c
+if [[ $SHELL ==  "/bin/zsh" ]];then
+	zle -N better_alt_c
+	bindkey '\et' better_alt_c
+elif [[ $SHELL == "/bin/bash" ]];then
+	bind -x '"\et"':better_alt_c
+fi
